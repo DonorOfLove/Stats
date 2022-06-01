ebc7756ac335609275e131a24ee3488e3f43b3f4
stas <StasEzovitov89@gmail.com>
2022-06-01 15:00:47 +0300

    0

--- components/Match/Match.js
+++ components/Match/Match.js
@@ -1,64 +1,126 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet, ScrollView, Image } from "react-native";
 import PlayerCard from "./PlayerCard";
 
 export default function App() {
 
   const [matches, setMatches] = useState({});
+  const [direTeam, setDireTeam] = useState([]);
+  const [radiantTeam, setRadiantTeam] = useState([]);
 
-  const [direTeam,setDireTeam]=useState([])
-  const [radiantTeam,setRadiantTeam]=useState([])
+  const [itemIds, setItemIds] = React.useState({});
+  const [items, setItems] = useState({});
 
-
-  function teamSort (players){
-    let radiant=[]
-    let dire=[]
-    for(let player of players){
-      if(player.isRadiant){
-        radiant.push(player)
-      }else{
-        dire.push(player)
+  function teamSort(players) {
+    let radiant = [];
+    let dire = [];
+    for (let player of players) {
+      if (player.isRadiant) {
+        radiant.push(player);
+      } else {
+        dire.push(player);
       }
     }
-    setRadiantTeam(radiant)
-    setDireTeam(dire)
+    setRadiantTeam(radiant);
+    setDireTeam(dire);
   }
 
   useEffect(() => {
-    fetch('https://api.opendota.com/api/matches/6588241024')
+    fetch("https://api.opendota.com/api/matches/6594429669")
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        setMatches(data)
-        teamSort(data.players)
+        setMatches(data);
+        teamSort(data.players);
+      });
+    fetch("https://api.opendota.com/api/constants/item_ids")
+      .then((response) => {
+        return response.json();
       })
+      .then((data) => {
+        setItemIds(data);
+      });
+    fetch("https://api.opendota.com/api/constants/items")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setItems(data);
+      });
   }, []);
 
   return (
-    <ScrollView >
-      {Object.entries(matches).length>0?(
-          <><Text>match id: {matches.match_id}</Text>
-            <Text>{matches.radiant_score}-{matches.dire_score}</Text>
-            <Text>{matches.radiant_win? 'Radiant':'Dire'} Win</Text>
-            <Text>Radiant Team:</Text>
-            <View>{radiantTeam.map((player, index)=>{
+    <ScrollView style={styles.root}>
+      {Object.entries(matches).length > 0 ? (
+          <View>
+            <Text style={styles.text}>match id: {matches.match_id}</Text>
+            <Text style={styles.text}>{`${Math.floor(matches.duration / 60)}:${matches.duration % 60}`}</Text>
+            <Text style={styles.text}>
+              <Text style={styles.radiant}>{matches.radiant_score}</Text>-<Text
+              style={styles.dire}>{matches.dire_score}</Text>
+            </Text>
+            <Text style={styles.text}>{matches.radiant_win ? "Radiant" : "Dire"} Win</Text>
+            <Text style={styles.radiant}>Radiant Team:</Text>
+
+            <ScrollView horizontal={true} contentContainerStyle={{
+              flexDirection: "column",
+            }}>
+              {/*<View style={styles.table}>*/}
+              {/*  <Text style={{ width: "30%", color: "#fff" }}>NAME</Text>*/}
+              {/*  <Text style={{ width: "25%", color: "#fff" }}>K/D/A</Text>*/}
+              {/*  <Text style={{ width: "30%", color: "#fff" }}>ITEMS</Text>*/}
+              {/*</View>*/}
+              {radiantTeam.map((player, index) => {
+                return (
+                  <PlayerCard player={player} key={player.id} items={items} itemIds={itemIds} />
+                );
+              })}
+            </ScrollView>
+            <Text style={styles.dire}>Dire Team:</Text>
+            <ScrollView horizontal={true} contentContainerStyle={{
+              flexDirection: "column",
+            }}>
+              {/*<View style={styles.table}>*/}
+              {/*  <Text style={{ width: "30%", color: "#fff" }}>NAME</Text>*/}
+              {/*  <Text style={{ width: "25%", color: "#fff" }}>K/D/A</Text>*/}
+              {/*  <Text style={{ width: "30%", color: "#fff" }}>ITEMS</Text>*/}
+              {/*</View>*/}
+              {direTeam.map((player, index) => {
               return (
-                <PlayerCard player={player} key={player.id}/>
-              )
+                <PlayerCard player={player} key={player.id} items={items} itemIds={itemIds} />
+              );
             })}
-            </View>
-            <Text>Dire Team:</Text>
-            <View>{direTeam.map((player, index)=>{
-              return (
-                <PlayerCard player={player} key={player.id}/>
-              )
-            })}
-            </View>
-          </>)
-        :<Image source={{uri:"https://www.google.com/search?q=cat+loading&client=opera-gx&tbm=isch&source=iu&ictx=1&vet=1&fir=kCf8-mUN82f7tM%252Csorzrv2AVsrrbM%252C_%253BgKVgsqzUoRHs0M%252Ce1GKW12_HBOxpM%252C_%253Bc0si6LcJh3Rg3M%252CkGUhCQsia4LQzM%252C_%253BROVxQkjOVfMpmM%252Cis0i1qFXwHrINM%252C_%253B7JU-dOyqNaAK6M%252Csorzrv2AVsrrbM%252C_%253Bs0p4deNm_erUsM%252CMqZ23scndMf2mM%252C_%253BpOVzCpejiVzIEM%252CrbNF25HUaqQSGM%252C_%253Bxa2IxPTCGD2glM%252CwB-fOR2BTwgMrM%252C_%253BaHVev3ZUVGAV_M%252CczljSH9C0Oa-1M%252C_%253BNqlv9ZWxbh6j8M%252CgerAgPg9yYdx0M%252C_%253BS9mu2B-KquhRYM%252Cuz0SOJe6erlFBM%252C_%253B4IgPrekgQClHLM%252Csorzrv2AVsrrbM%252C_%253BPo2FHICnu2qBXM%252CGhqndHZN5UgvaM%252C_%253BRoT1YjzgcP7--M%252CczljSH9C0Oa-1M%252C_%253B8OUB1cItRSl3NM%252C4_mxGIV9xYQTGM%252C_&usg=AI4_-kRzENVyJsS142JSB25DHaiontQLLw&sa=X&ved=2ahUKEwivnui81Yn4AhWJM-wKHbZLCNQQ9QF6BAgREAE&biw=1366&bih=665&dpr=1#imgrc=gKVgsqzUoRHs0M"}}/>}
+            </ScrollView>
+          </View>)
+        : <Image
+          source={{ uri: "https://www.google.com/search?q=cat+loading&client=opera-gx&tbm=isch&source=iu&ictx=1&vet=1&fir=kCf8-mUN82f7tM%252Csorzrv2AVsrrbM%252C_%253BgKVgsqzUoRHs0M%252Ce1GKW12_HBOxpM%252C_%253Bc0si6LcJh3Rg3M%252CkGUhCQsia4LQzM%252C_%253BROVxQkjOVfMpmM%252Cis0i1qFXwHrINM%252C_%253B7JU-dOyqNaAK6M%252Csorzrv2AVsrrbM%252C_%253Bs0p4deNm_erUsM%252CMqZ23scndMf2mM%252C_%253BpOVzCpejiVzIEM%252CrbNF25HUaqQSGM%252C_%253Bxa2IxPTCGD2glM%252CwB-fOR2BTwgMrM%252C_%253BaHVev3ZUVGAV_M%252CczljSH9C0Oa-1M%252C_%253BNqlv9ZWxbh6j8M%252CgerAgPg9yYdx0M%252C_%253BS9mu2B-KquhRYM%252Cuz0SOJe6erlFBM%252C_%253B4IgPrekgQClHLM%252Csorzrv2AVsrrbM%252C_%253BPo2FHICnu2qBXM%252CGhqndHZN5UgvaM%252C_%253BRoT1YjzgcP7--M%252CczljSH9C0Oa-1M%252C_%253B8OUB1cItRSl3NM%252C4_mxGIV9xYQTGM%252C_&usg=AI4_-kRzENVyJsS142JSB25DHaiontQLLw&sa=X&ved=2ahUKEwivnui81Yn4AhWJM-wKHbZLCNQQ9QF6BAgREAE&biw=1366&bih=665&dpr=1#imgrc=gKVgsqzUoRHs0M" }} />}
     </ScrollView>
   );
 }
 const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    padding: 10,
+    flexGrow: 1,
+    backgroundColor: "#180b25",
+  },
+  table: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-around",
+  },
+
+  dire: {
+    color: "#8b1717",
+    fontSize: 25,
+  },
+  radiant: {
+    color: "#49ac4a",
+    fontSize: 25,
+  },
+  text: {
+    color: "#fff",
+    fontSize: 25,
+  },
 });
--- components/Match/PlayerCard.js
+++ components/Match/PlayerCard.js
@@ -1,50 +1,127 @@
 import React, { Component, useState } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
-import _ from "lodash"
+import { View, Text, StyleSheet, Image, ScrollView} from "react-native";
+import _ from "lodash";
 
-const playerCard = ({ player }) => {
+const playerCard = ({ player,items,itemIds }) => {
 
-  const [hero,setHero]=React.useState({})
-  const [items,setItems]=useState({})
+  const [hero, setHero] = React.useState({});
 
-  React.useEffect(()=> {
-    fetch('https://api.opendota.com/api/constants/heroes')
+  React.useEffect(() => {
+    fetch("https://api.opendota.com/api/constants/heroes")
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        setHero(data[player.hero_id])
-      })
-    fetch('https://api.opendota.com/api/constants/item_ids')
-      .then((response)=>{
-        return response.json();
-      })
-      .then((data)=>{
-        setItems(data)
-      })},[])
+        setHero(data[player.hero_id]);
+      });
 
-    return (
-      <View style={styles.main}>
-        {!_.isEmpty(hero)?(
-          <Image source={{uri:`https://cdn.cloudflare.steamstatic.com/${hero.icon}`}}
-                 style={{width:50, height:50}}/>
-        ):(
-          <Image source={{uri:  "https://http.cat/102"}}
-                 style={{width:50, height:50}}/>
+  }, []);
+
+  return (
+    <View style={styles.main}>
+      {!_.isEmpty(hero) ? (
+        <Image source={{ uri: `https://cdn.cloudflare.steamstatic.com/${hero.icon}` }}
+               style={styles.image} />
+      ) : (
+        <Image source={{ uri: "https://http.cat/102" }}
+               style={styles.image} />
+      )}
+      <Text style={styles.info}>{player.level} {player.personaname || "Anon"}</Text>
+
+      <Text style={styles.KDA}> {player.kills}/{player.deaths}/{player.assists}  </Text>
+
+      <View style={styles.items}>
+      {!_.isEmpty(items)&&player.item_0!==0 ? (
+          <Image source={{ uri: `https://cdn.cloudflare.steamstatic.com/${items[itemIds[player.item_0]]['img']}`}}
+                 style={styles.image} />)
+        : (
+          null
         )}
-        <Text>{player.level} {player.personaname||"Anon"}</Text>
-        <Text> {player.kills}/{player.deaths}/{player.assists}</Text>
-        <Image source={{uri:`https://cdn.cloudflare.steamstatic.com/${items[player.item_0].img}`}}
-        style={{width:50,height:50}}/>
+        {!_.isEmpty(items)&&player.item_1!==0 ? (
+            <Image source={{ uri: `https://cdn.cloudflare.steamstatic.com/${items[itemIds[player.item_1]]['img']}`}}
+                   style={styles.image} />)
+          : (
+            null
+          )}
+        {!_.isEmpty(items)&&player.item_2!==0 ? (
+            <Image source={{ uri: `https://cdn.cloudflare.steamstatic.com/${items[itemIds[player.item_2]]['img']}`}}
+                   style={styles.image} />)
+          : (
+            null
+          )}
+        {!_.isEmpty(items)&&player.item_3!==0 ? (
+            <Image source={{ uri: `https://cdn.cloudflare.steamstatic.com/${items[itemIds[player.item_3]]['img']}`}}
+                   style={styles.image} />)
+          : (
+            null
+          )}
+        {!_.isEmpty(items)&&player.item_4!==0 ? (
+            <Image source={{ uri: `https://cdn.cloudflare.steamstatic.com/${items[itemIds[player.item_4]]['img']}`}}
+                   style={styles.image} />)
+          : (
+            null
+          )}
+        {!_.isEmpty(items)&&player.item_5!==0 ? (
+            <Image source={{ uri: `https://cdn.cloudflare.steamstatic.com/${items[itemIds[player.item_5]]['img']}`}}
+                   style={styles.image} />)
+          : (
+            null
+          )}
       </View>
-    );
-}
+      <Text style={styles.net}>{player.net_worth}</Text>
+      <Text style={styles.lh}>{player.last_hits}</Text>
+      <Text style={styles.dn}>{player.denies}</Text>
+      <Text style={styles.gpm}>{player.gold_per_min}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-main:{
-  display:'flex',
-  flexDirection:'row'
-}
+  main: {
+    flexDirection: "row",
+    padding:10,
+  },
+  image:{
+    width:30,
+    height:30,
+    marginRight:5,
+  },
+  KDA:{
+    color:'#fff',
+    fontSize:20,
+    width:125
+  },
+  info:{
+    color:'#fff',
+    fontSize:15,
+    width:175
+  },
+  items:{
+    flexDirection:'row',
+    alignSelf:'flex-end',
+    width:250,
+  },
+  net:{
+    color:'#fadd4f',
+    width:100,
+    fontSize:15,
+  },
+  lh:{
+    color:'#fff',
+    width:30,
+    fontSize:15,
+  },
+  dn:{
+    color:'#fff',
+    width:30,
+    fontSize:15,
+  },
+  gpm:{
+    color:'#fff',
+
+    fontSize:15,
+  }
+
 });
 
-export default playerCard
+export default playerCard;
